6d27d47bb242e2f1c33d54e0959831fe76293012
Sahaj Dubey <dubeysahaj81@gmail.com>
2025-10-30 15:38:49 +0530

    10. Conditional Statement

---
+++ Downloads/Screenshots/New folder (2)/New folder/Java Programming Language/10. Conditional Statement/WeeksPrint_Switch.java
@@ -0,0 +1,27 @@
+/*write a java program to input week number(1-7) and print day of week name using Switch */
+import java.util.*;
+public class WeeksPrint_Switch {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        /*input week number from user */
+        System.out.println("Enter week number (1-7):");
+        int week = sc.nextInt();
+        switch(week) {
+            case 1 : System.out.println("Monday");
+                      break;
+            case 2 : System.out.println("Tuesday");
+                      break;
+            case 3 : System.out.println("Wednesday");
+                      break;
+            case 4 : System.out.println("Thursday");
+                      break;
+            case 5 : System.out.println("Friday");
+                      break;
+            case 6 : System.out.println("Saturday");
+                      break;
+            case 7 : System.out.println("Sunday");
+                      break;
+            default : System.out.println("Invalid input: please enter week number(1-7)");                            
+        }
+    }
+}
